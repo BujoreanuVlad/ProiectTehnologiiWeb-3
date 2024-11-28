9358f7f1f578b5455869ba4beeeebb55e082b0c2
Vlad Bujoreanu <vladbujorean@gmail.com>
2024-11-28 21:37:36 +0200

    Set up webpack and React

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const cors = require('cors')
+const path = require("path");
+
+const router = express.Router()
+const app = express()
+
+app.use(cors())
+app.use(express.json());
+app.use(express.static("dist"));
+
+app.use("", router)
+
+router.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "dist/index.html"));
+})
+
+app.listen(3000, () => {console.log("Listening on port:", 3000)})
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './app.js', // Entry point of your application
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js', // Output bundle file name
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // Use Babel for .js and .jsx files
+                },
+            },
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html', // Use this HTML file as a template
+        }),
+    ],
+};

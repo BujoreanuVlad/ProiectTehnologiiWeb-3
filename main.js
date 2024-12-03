6c6c6281906d1e81a7ef714ed591e4b335e27fc7
Paula <paula@Paulas-MacBook-Pro.local>
2024-12-03 14:48:45 +0200

    Modificare main.js

--- main.js
+++ main.js
@@ -16,4 +16,4 @@ router.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "dist/index.html"));
 })
 
-app.listen(3000, () => {console.log("Listening on port:", 3000)})
+app.listen(3000, () => {console.log("Server started on: " + "http://localhost:3000")})

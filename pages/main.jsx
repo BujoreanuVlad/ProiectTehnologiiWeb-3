dc1a2b85cc93f5a3ca89be2a458ae52b374e69bd
paulinaBo <bolotovicipaulina22@stud.ase.ro>
2024-12-15 18:05:12 +0200

    Am adaugat pagina de admin

--- pages/main.jsx
+++ pages/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-import App from './login/app.jsx'
-import '../index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import "../index.css";
+import AppRoutes from "./_routes/route.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppRoutes />
+  </StrictMode>
+);
